34914246736077acc3ca005e1e30cf058c3e9def
Jibin Baby <jibinb961@gmail.com>
2025-01-14 16:43:21 -0500

    Added code for getting the formatted data from the random user api

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -8,14 +8,40 @@ default_args = {
     'start_date': datetime(2025,1,14,10,00)
 }
 
-def stream_data():
-    import json
+def get_data():
     import requests
 
     res= requests.get('https://randomuser.me/api/')
     res = res.json()
     res = res['results'][0] # just to make sure that we take the first entry from the results json, even though we just have one result.
+    return res
+
+def format_data(res):
+    data  = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, {location['city']}, {location['state']}, {location['country']}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
+
+
+def stream_data():
+    import json
+    res = get_data()
+    res = format_data(res)
     print(json.dumps(res,indent=3))
+    
 
 
 # with DAG('user_automation',
